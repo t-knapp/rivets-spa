f41d904deb5717807aa4e5a1a1bd36d0533a1945
t-knapp <tobias-knapp@web.de>
2018-02-01 21:15:26 +0100

    Added delete

--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ var Application = (function(rivets, $, randomstring) {
             add: function(event, boundModel) {
                 //boundModel.model.items.push({title: 'Title', text: randomstring.generate()})
                 addItem();
+            },
+            remove: function(event) {
+                var id = event.target.getAttribute('data-id');
+                deleteItem(id);
             }
         },
         removeClass: 'hide'
@@ -62,6 +66,23 @@ var Application = (function(rivets, $, randomstring) {
             console.warn('e', e);
             model.list.addPossible = true;
         })
+    };
+
+    var deleteItem = function(docId) {
+        pouchDB.get(docId)
+        .then(function (doc) {
+            return pouchDB.remove(doc._id, doc._rev);
+        })
+        .then(function(result) {
+            var i;
+            var found = false;
+            for(i = 0; i < model.list.items.length && !found; i++) {
+                if(model.list.items[i]._id === docId) {
+                    found = true;
+                    model.list.items.splice(i, 1);
+                }
+            }
+        });
     };
 
     rivets.configure({
